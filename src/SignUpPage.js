cc769b43b36b9450a5fb6e710f94d503f27f00e8
aj79ysid <mohammed.ughratdar@fau.de>
2023-06-13 21:39:43 +0200

    Added the Signup page CSS

--- src/SignUpPage.js
+++ src/SignUpPage.js
@@ -1,84 +1,93 @@
-import { useMutation } from "@tanstack/react-query"
+import { useMutation } from "@tanstack/react-query";
 import useForm from './useForm';
-import { createUser } from "./api/users"
-import { useNavigate } from 'react-router-dom'
+import { createUser } from "./api/users";
+import { useNavigate } from 'react-router-dom';
+import './Designs/SignUpPage.css';
 
 const SignUpPage = () => {
-
     const navigate = useNavigate();
 
     const createSignUp = useMutation({
         mutationFn: createUser,
         onSuccess: data => {
-        
             console.log(data);
-
             navigate('/Login');
         },
-    })
-
+    });
 
     const { values, errors, handleChange, handleSubmit } = useForm(
-        { username: '', password: '', name: '', dob: '', address: '', description: ''},
+        { name: '', username: '', password: '', dob: '', city: '', designation: '' },
         (data) => {
             console.log(data);
             createSignUp.mutate({ data });
         },
+        (data) => { }
+        );
 
-        (data) => {
+        return (
+            <div className="signup-container">
+                <form onSubmit={handleSubmit} className="signup-form">
+                    <h2 className="signup-title">Sign Up for Friends' App</h2>
 
+                        <input
+                            type="text"
+                            name="name"
+                            value={values.name}
+                            onChange={handleChange}
+                            className="signup-input"
+                            placeholder="Name"
+                        />
+                        <input
+                            type="text"
+                            name="username"
+                            value={values.username}
+                            onChange={handleChange}
+                            className="signup-input"
+                            placeholder="Username"
+                        />
+                        <input
+                            type="password"
+                            name="password"
+                            value={values.password}
+                            onChange={handleChange}
+                            className="signup-input"
+                            placeholder="Password"
+                        />
+                    <input
+                        type="text"
+                        name="dob"
+                        value={values.dob}
+                        onChange={handleChange}
+                        className="signup-input"
+                        placeholder="Date of Birth (dd/mm/yyyy)"
+                        onFocus={(e) => (e.target.type = "date")}
+                        onBlur={(e) => (e.target.type = "text")}
+                    />
+                    
+                        <input
+                            type="text"
+                            name="City"
+                            value={values.address}
+                            onChange={handleChange}
+                            className="signup-input"
+                            placeholder="City"
+                        />
+                        <input
+                            type="text"
+                            name="designation"
+                            value={values.description}
+                            onChange={handleChange}
+                            className="signup-input"
+                            placeholder="Designation"
+                        />
+                        <button type="submit" className="signup-button">Sign Up</button>
+                           <div className="already-signed-up"> Already Signed up? </div>
+                        <button onClick={() => navigate('/Login')} className="login-button">Log In</button>
+                 </form>
 
-        }
+             </div>
+      
     );
-
-    return (
-        <>
-        <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                name="username"
-                value={values.username}
-                onChange={handleChange}
-            />
-            <input
-                type="password"
-                name="password"
-                value={values.password}
-                onChange={handleChange}
-            />
-            <input
-                type="text"
-                name="name"
-                value={values.name}
-                onChange={handleChange}
-            />
-            <input
-                type="date"
-                name="dob"
-                value={values.dob}
-                onChange={handleChange}
-            />
-            <input
-                type="text"
-                name="address"
-                value={values.address}
-                onChange={handleChange}
-            />
-            <input
-                type="text"
-                name="description"
-                value={values.description}
-                onChange={handleChange}
-            />
-            <button type="submit">Submit</button>
-            
-        </form>
-
-        <button onClick={()=> navigate('/Login')}>Log In</button>
-
-        </>
-    );
-
 };
 
 export default SignUpPage;
