d4747aa7f0cd5e2a3b842e624ee9f3ab7d95229c
aj79ysid <mohammed.ughratdar@fau.de>
2023-06-14 20:18:51 +0200

    Added the Login page CSS and also added header and the footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import UserIndex from './UserIndex'
 import { useEffect } from 'react'
 import { Route, Routes, useNavigate, useLocation } from 'react-router-dom'
 import GenerateNewToken from './RefreshToken'
+import { FaEnvelope, FaPhone, FaGithub } from 'react-icons/fa';
 
 export default function App() {
 
@@ -43,11 +44,54 @@ export default function App() {
 
 
     return (
+
+        <div className='App'>
+        <div className="github-links">
+                <a href="https://github.com/mohmaaya/wirecraft_be" target="_blank" rel="noopener noreferrer">
+            <span className="link-icon">
+                <FaGithub size={24} />
+            </span>
+            BackEnd Code
+        </a>
+
+                <a href="https://github.com/mohmaaya/wirecraft_fe" target="_blank" rel="noopener noreferrer">
+            <span className="link-icon">
+                <FaGithub size={24} />
+            </span>
+            FrontEnd Code
+        </a>
+        </div>
+
+
         <Routes>
             <Route path='/' />
             <Route path='/SignUp' element={<SignUpPage />} />
             <Route path='/Login' element={<LoginPage />} />
             <Route path='/Home' element={<UserIndex />} />
-        </Routes>
+            </Routes>
+
+            <div className='footer'>
+                <span className="moving-text">
+                    This Friends' web app is developed by Mohammed Ughratdar as a portfolio work.
+                    Mohammed is looking for a fulltime position as a Developer.
+                    He is skilled in both backend and frontend.
+                    You may reach out to him using the contact details given below.
+                </span>
+            </div>
+
+            <div className="contact-icons">
+                
+                <a href="mailto:mohammed.ughratdar@fau.de">
+                    <FaEnvelope size={24} />
+                    mohammed.ughratdar@fau.de
+                </a>
+
+                <span>
+                    <FaPhone size={24} />
+                    +49 151 6868 0236
+                </span>
+            </div>
+
+        </div>
     );
 }
--- src/LoginPage.js
+++ src/LoginPage.js
@@ -2,6 +2,7 @@ import { useMutation } from "@tanstack/react-query";
 import useForm from "./useForm";
 import { login } from "./api/users";
 import { useNavigate } from "react-router-dom";
+import "./Designs/LoginPage.css";
 
 const LoginPage = () => {
     const navigate = useNavigate();
@@ -9,10 +10,8 @@ const LoginPage = () => {
     const createLogin = useMutation({
         mutationFn: login,
         onSuccess: (data) => {
-        
             localStorage.setItem("loggedUsername", data.username);
             localStorage.setItem("access_token", data.access_token);
-            
             navigate("/Home");
         },
         config: {
@@ -29,25 +28,31 @@ const LoginPage = () => {
     );
 
     return (
-        <>
-            <form onSubmit={handleSubmit}>
+        <div className="login-container">
+            <form className="login-form" onSubmit={handleSubmit}>
+                <h2 className="login-title">Login to the Friends' App</h2>
                 <input
+                    className="login-input"
                     type="text"
                     name="username"
                     value={values.username}
                     onChange={handleChange}
+                    placeholder="Username"
                 />
                 <input
+                    className="login-input"
                     type="password"
                     name="password"
                     value={values.password}
                     onChange={handleChange}
+                    placeholder="Password"
                 />
-                <button type="submit">Submit</button>
+                <button className="login-button" type="submit">Submit</button>
+                <div className="sign-up"> New User? Sign up</div>
+                <button className="login-button" onClick={() => navigate("/SignUp")}>Sign Up</button>
             </form>
 
-            <button onClick={() => navigate("/SignUp")}>Sign Up</button>
-        </>
+        </div>
     );
 };
 
